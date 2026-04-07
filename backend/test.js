40c575d24c3ab36722f9251f3b9b459d4a865e3b
Aman Kumar <126275690+KrAman66@users.noreply.github.com>
2026-04-07 23:34:49 +0530

    changes

--- backend/test.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect(
-  "mongodb+srv://Kr_Aman66:93RT1qpEfzM8EtkP@cluster0.xv7w0.mongodb.net/paytm_demo",
-);
-
-mongoose.connection.on("connected", () => {
-  console.log("MongoDB connected");
-});
